0c6b04fa05e95e48d1c32444efed4e2dfb6325de
usmanabbas2211 <usmanabbas2211@gmail.com>
2022-10-19 13:42:11 -0400

    added hello api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
-const { authenticate } = require("./middlewares/auth")
+const { authenticate } = require('./middlewares/auth')
 const userRoutes = require('./routes/userRoutes')
 const taskRoutes = require('./routes/taskRoutes')
 require('colors')
@@ -11,15 +11,17 @@ const app = express()
 app.use(express.json())
 
 app.use(morgan('combined'))
-
+app.get('/api/hello', (req, res) => {
+    res.send('Hello World!')
+})
 app.use('/api', userRoutes)
-app.use('/api/tasks',authenticate ,taskRoutes)
+app.use('/api/tasks', authenticate, taskRoutes)
 
 mongoose
-	.connect(process.env.MONGO_QUERY_STRING)
-	.then(() => console.log('connected to mongoDB'.white.bgBlue))
-	.catch(err => console.log(err.red))
+    .connect(process.env.MONGO_QUERY_STRING)
+    .then(() => console.log('connected to mongoDB'.white.bgBlue))
+    .catch((err) => console.log(err.red))
 
 app.listen(process.env.PORT, () =>
-	console.log(`server is listening on port ${process.env.PORT}`.bgCyan)
+    console.log(`server is listening on port ${process.env.PORT}`.bgCyan)
 )
